f971cbcc1c6592123e48728b85f0617a4a04ac6a
Guilherme Sobreira <gui.sobreira10@gmail.com>
2024-11-22 22:32:04 -0300

    Casos de teste para criar as mudancas feitas ate o momento

--- src/main.c
+++ src/main.c
@@ -4,8 +4,18 @@
 #include "fila.h"
 #include "utils.h"
 
+#define BIBLIOTECA_IMPLEMENTATION
+#define LISTA_IMPLEMENTATION
+
 int main()
 {
-    printf("Oi");
+    LIVRO *teste = create_LIVRO("titulo", "Guilherme", "978-8533613409", 2024);
+    LIVRO *teste2 = create_LIVRO("titulo2", "Andre", "123-4567891012", 1999);
+
+    List l = {0};
+
+    insert_LISTA(&l, teste, 1);
+    insert_LISTA(&l, teste2, 2);
+    print_LISTA(&l);
     return 0;
 }
