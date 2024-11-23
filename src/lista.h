c5e4dee5f866c7f50402b0899473c5c83627d12b
Guilherme Sobreira <gui.sobreira10@gmail.com>
2024-11-22 22:31:33 -0300

    linked list implementada

--- src/lista.h
+++ src/lista.h
@@ -3,19 +3,95 @@
 
 #include "biblioteca.h"
 
-typedef struct ELEMENTO_LISTA
-{
-    LIVRO livro;
-    struct ELEMENTO_LISTA* prox;
-} ELEMENTO_LISTA;
+typedef struct node{
+    struct node *next; // 8 bytes bytes
+    LIVRO *data; // 8 bytes
+} Node;
 
-typedef struct LISTA 
-{
-    ELEMENTO_LISTA* inicio;
-    int tamanho;
-} LISTA;
+typedef struct {
+    Node *head;
+} List;
 
 // Funções para manipular a LISTA
 
+#ifdef LISTA_IMPLEMENTATION
 
+bool insert_LISTA(List *l, LIVRO *data, size_t pos)
+{
+    Node* new = (Node *) malloc(sizeof(Node));
+    
+    if (new == NULL) {
+        print_error(MALLOC_ERR);
+        return false;
+    }
+    
+    new->data = data;
+
+    if (pos == 1) {
+        l->head = new;
+        return true;
+    }
+    
+    Node *temp = l->head;
+ 
+    for (size_t i = 1; temp != NULL && i < pos-1; ++i) {
+        temp = temp->next;
+    }
+    
+    if (temp == NULL) {
+        print_error(OUT_OF_RANGE);
+        free(new);
+        return false;
+    }
+ 
+    new->next = temp->next;
+    temp->next = new;
+    return true;
+}
+
+bool delete_LISTA(List *l, LIVRO *livro, size_t pos)
+{
+    if (l->head == NULL) {
+        print_error(EMPTY_LIST);
+        return false;
+    }
+    Node *temp = l->head;
+    if (pos == 1) {
+        *livro = *(temp->data);
+        l->head = temp->next;
+        free(temp);
+        return true;
+    }
+    Node *prev;
+    for (size_t i = 1; temp != NULL && i < pos-1; ++i) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        print_error(OUT_OF_RANGE);
+        return false;
+    }
+    
+    *livro = *(temp->data);
+    prev->next = temp->next;
+    free(temp);
+    return true;
+    
+}
+
+void print_LISTA(List *l)
+{
+    Node *n = l->head;
+    int i = 1;
+    while (n != NULL) {
+        printf("*---------------------*\n");
+        printf("Elemento %d:\n", i);
+        print_LIVRO(*(n->data));
+        n = n->next;
+        i++;
+    }
+}
+
+#endif // LISTA_IMPLEMENTATION
 #endif // LISTA_H
