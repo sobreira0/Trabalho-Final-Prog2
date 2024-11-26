0fe1b87d5beffc5eab6a9c0c25cff2f38814dd27
Guilherme Sobreira <gui.sobreira10@gmail.com>
2024-11-25 22:05:25 -0300

    testes implementados na main

--- src/main.c
+++ src/main.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "biblioteca.h"
-#include "lista.h"
-#include "utils.h"
-
 #define BIBLIOTECA_IMPLEMENTATION
 #define LISTA_IMPLEMENTATION
 #define UTILS_IMPLEMENTATION
 
+#include "biblioteca.h"
+#include "lista.h"
+#include "utils.h"
+
+
+
 int main() {
     // Criação da lista
-    List minhaLista = {NULL};
+    List minhaLista = {0};
 
     // Livros para adicionar na lista
-    LIVRO livro1 = {"O Senhor dos Aneis", "J.R.R. Tolkien", 1954, "1234567890123", 1}; // true
-    LIVRO livro2 = {"O Hobbit", "J.R.R. Tolkien", 1937, "9876543210123", 1}; // true
-    LIVRO livro3 = {"A Revolucao dos Bichos", "George Orwell", 1945, "1928374650123", 1}; // true
-    LIVRO livro4 = {"1984", "George Orwell", 1949, "5647382910123", 1}; // true
+    LIVRO *livro1 = create_LIVRO("Senhor dos Aneis", "J.R.R. Tolkien", "123-4567890123", 1954);
+    LIVRO *livro2 = create_LIVRO("O Hobbit", "J.R.R. Tolkien", "987-6543210123", 1937); 
+    LIVRO *livro3 = create_LIVRO("A Revolucao dos Bichos", "George Orwell", "192-8374650123", 1945);
+    LIVRO *livro4 = create_LIVRO("1984", "George Orwell", "564-7382910123", 1949);
 
     // Inserção dos livros na lista
-    insert_LISTA(&minhaLista, &livro1, 1);
-    insert_LISTA(&minhaLista, &livro2, 2);
-    insert_LISTA(&minhaLista, &livro3, 3);
-    insert_LISTA(&minhaLista, &livro4, 4);
+    insert_LISTA(&minhaLista, livro1, 0);
+    insert_LISTA(&minhaLista, livro2, 1);
+    insert_LISTA(&minhaLista, livro3, 2);
+    insert_LISTA(&minhaLista, livro4, 3);
 
     // Imprimir lista antes da ordenação
     printf("Lista antes de ordenar:\n");
@@ -31,11 +30,11 @@ int main() {
 
     // Ordena a lista por título (merge sort)
     
-    mergeSort_LISTA(&minhaLista.head);
+    // mergeSort_LISTA(&minhaLista.head);
     
-    // Imprimir lista após a ordenação
-    printf("\nLista apos ordenar por titulo:\n");
-    print_LISTA(&minhaLista);
+    // // Imprimir lista após a ordenação
+    // printf("\nLista apos ordenar por titulo:\n");
+    // print_LISTA(&minhaLista);
 
     return 0;
 }
