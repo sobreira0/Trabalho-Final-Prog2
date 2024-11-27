2d143dfe4ae2bb51f3017bb9ee41c9bddaec7342
Lucas Brandao <lucas.mbrandao2006@gmail.com>
2024-11-27 14:20:39 -0300

    Adicionei mais funcoes na fila

--- src/fila.h
+++ src/fila.h
@@ -3,29 +3,34 @@
 
 #include "biblioteca.h"
 
-typedef struct ELEMENTO_FILA {
+typedef struct ELEMENTO_FILA 
+{
     PESSOA pessoa;
     struct ELEMENTO_FILA* prox;
 } ELEMENTO_FILA;
 
-typedef struct FILA {
+typedef struct FILA 
+{
     ELEMENTO_FILA* frente;
     ELEMENTO_FILA* tras;
     int tamanho;
 } FILA;
 
-typedef struct PESSOA {
+typedef struct PESSOA 
+{
     char nome[MAX];
 } PESSOA;
 
 // Funções para manipular a fila
-void inicializarFila(FILA *f){
+void inicializarFila(FILA *f)
+{
     f->frente = NULL;
     f->tras = NULL;
     f->tamanho = 0;
 }
 
- void enfileiramento(FILA *f, PESSOA p){
+void enfileiramento(FILA *f, PESSOA p) 
+{
     ELEMENTO_FILA *novo = malloc(sizeof(ELEMENTO_FILA));
     if(novo == NULL) {
         printf("Erro de alocacao de memoria\n");
@@ -40,5 +45,32 @@ void inicializarFila(FILA *f){
         }
         f->tras = novo;
     }
- }
+    f->tamanho++;
+}
+
+PESSOA desenfileiramento(FILA *f) 
+{
+    ELEMENTO_FILA *ptr = f->frente;
+    PESSOA p_escolhida;
+    if(ptr == NULL) {
+        printf("Fila vazia");
+        return;
+    } else {
+        f->frente = f->frente->prox;
+        ptr->prox = NULL;
+        p_escolhida= ptr->pessoa;
+        free(ptr);
+        if(f->frente == NULL) {
+            f->tras = NULL;
+        }
+        return p_escolhida;
+    }
+    f->tamanho--;
+}
+
+int tamanhoFila(FILA *f) 
+{
+    return f->tamanho;
+}
+ 
 #endif // FILA_H
