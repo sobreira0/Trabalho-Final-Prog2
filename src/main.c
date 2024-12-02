0e92f96ac936e2460f169e75405ae399914ed09e
Guilherme Sobreira <gui.sobreira10@gmail.com>
2024-12-02 13:37:18 -0300

    descomentei a main

--- src/main.c
+++ src/main.c
@@ -34,61 +34,60 @@ int main()
         insert_LISTA(&livros, *livro, contador);
         contador++;
     }
-    print_LISTA(&livros);
     /**
      * Primeira versao do menu, ainda a ser implementado.
      */
-    // puts("Digite seu nome:");
-    // char nome[100] = {0};
-    // scanf("%s", nome);
-    // puts("Digite seu CPF:");
-    // char cpf[100] = {0};
-    // scanf("%s", cpf);
-    // PESSOA *p = cria_PESSOA(nome, cpf);
-    // if(!adiciona_FILA(&pessoas, *p)) return 1;
+    puts("Digite seu nome:");
+    char nome[100] = {0};
+    scanf("%s", nome);
+    puts("Digite seu CPF:");
+    char cpf[100] = {0};
+    scanf("%s", cpf);
+    PESSOA *p = cria_PESSOA(nome, cpf);
+    if(!adiciona_FILA(&pessoas, *p)) return 1;
     
-    // getchar();
-    // char opt = -1;
-    // while (opt != 'q') {
-    //     puts("<-------------------------------->");
-    //     puts("\tBem vindo a Biblioteca BCC");
-    //     puts("\tDigite 'q' para sair");
-    //     puts("\tEscolha uma opcao:");
-    //     puts("\t1 - Consultar meu livro emprestado");
-    //     puts("\t2 - Cadastrar livro");
-    //     puts("\t3 - Listar livros em ordem alfabetica de titulo");
-    //     scanf("%c", &opt);
-    //     switch (opt)
-    //     {
-    //     case '1':
-    //         if (p->livro_emprestado == NULL) puts("Voce nao tem livros emprestados no momento");
-    //         else print_LIVRO(*p->livro_emprestado);
-    //         break;
-    //     case '2': ;
-    //         char titulo[TITULO_MAX], autor[AUTOR_MAX];
-    //         int ano_publicacao;
-    //         puts("Digite o titulo do livro:");
-    //         scanf("%s", titulo);
-    //         puts("Digite o autor do livro:");
-    //         scanf("%s", autor);
-    //         puts("Digite o ano de lancamento do livro");
-    //         scanf("%d", &ano_publicacao);
-    //         char ISBN[ISBN_MAX];
-    //         puts("Digite a ISBN do livro que deseja pegar emprestado");
-    //         scanf("%s", ISBN);
-    //         LIVRO *livro = create_LIVRO(titulo, autor, ISBN, ano_publicacao);
-    //         if (livro == NULL) return 1;
-    //         if(!escrever_livro(arquivo_livro, livro)) return 1;
-    //         break;
-    //     case '3': ;
-    //         List livros_sorted;
-    //         memcpy(&livros_sorted, &livros, sizeof(List));
-    //         mergeSort_LISTA(&(livros_sorted.head));
-    //         print_LISTA(&livros_sorted);
-    //         break;
-    //     }
-    //     getchar();
-    // } 
+    getchar();
+    char opt = -1;
+    while (opt != 'q') {
+        puts("<-------------------------------->");
+        puts("\tBem vindo a Biblioteca BCC");
+        puts("\tDigite 'q' para sair");
+        puts("\tEscolha uma opcao:");
+        puts("\t1 - Consultar meu livro emprestado");
+        puts("\t2 - Cadastrar livro");
+        puts("\t3 - Listar livros em ordem alfabetica de titulo");
+        scanf("%c", &opt);
+        switch (opt)
+        {
+        case '1':
+            if (p->livro_emprestado == NULL) puts("Voce nao tem livros emprestados no momento");
+            else print_LIVRO(p->livro_emprestado);
+            break;
+        case '2': ;
+            char titulo[TITULO_MAX], autor[AUTOR_MAX];
+            int ano_publicacao;
+            puts("Digite o titulo do livro:");
+            scanf("%s", titulo);
+            puts("Digite o autor do livro:");
+            scanf("%s", autor);
+            puts("Digite o ano de lancamento do livro");
+            scanf("%d", &ano_publicacao);
+            char ISBN[ISBN_MAX];
+            puts("Digite a ISBN do livro que deseja pegar emprestado");
+            scanf("%s", ISBN);
+            LIVRO *livro = create_LIVRO(titulo, autor, ISBN, ano_publicacao);
+            if (livro == NULL) return 1;
+            if(!escrever_livro(arquivo_livro, livro)) return 1;
+            break;
+        case '3': ;
+            List livros_sorted;
+            memcpy(&livros_sorted, &livros, sizeof(List));
+            mergeSort_LISTA(&(livros_sorted.head));
+            print_LISTA(&livros_sorted);
+            break;
+        }
+        getchar();
+    } 
 
     fclose(arquivo_livro);
 
